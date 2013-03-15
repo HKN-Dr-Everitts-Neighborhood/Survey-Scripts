0398e87314773db4494b0d47fa7baee2e7a6d524
David Goldstein <dgoldstein0@gmail.com>
2013-03-15 01:28:45 -0500

    wrote closed_to_draft script, which moves all closed surveys to the editing phase.

--- closed_to_draft.py
+++ closed_to_draft.py
@@ -8,35 +8,15 @@ from secrets import netid, password
 
 import unittest, time, re
 
-text_field_ids = {
-    "viewer": "viewer_add",
-    "editor": "editor",
-    "admin": "admin_add"
-}
-
-button_ids = {
-    "viewer": "results_view_add",
-    "editor": "editor_add",
-    "admin": "admin_add_user"
-}
-
-class AddUser(unittest.TestCase):
+class DraftSurveys(unittest.TestCase):
     def setUp(self):
-        # Get user input
-        self.new_user = raw_input("netid of new user: ")
-        
-        self.new_user_type = ""
-        while (self.new_user_type not in text_field_ids):
-            self.new_user_type = raw_input("user type (editor, admin, or viewer): ")
-        
-        # Now open Firefox
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
         self.base_url = "https://illinois.edu/"
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_add_user(self):
+    def test_draft_surveys(self):
         driver = self.driver
         driver.get(self.base_url + "toolbox")
         
@@ -48,30 +28,28 @@ class AddUser(unittest.TestCase):
         driver.find_element_by_id("Password").send_keys(password)
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
 
-        # Go to reports tab
+        # Go to setup tab
         driver.find_element_by_id("survey-setup").click()
 
-        num_elements = len(driver.find_elements_by_css_selector("td.t-col-role"))
+        num_elements = len(driver.find_elements_by_css_selector("td.t-col-edit-form > a"))
+        
+        # so it turns out that only closed survyes have the t-col-edit-form class; hence why we are using
+        # element 0 at each iteration
         for i in xrange(0, num_elements):
-            # select roles for the next survey
+            # click edit form
             try:
-                driver.find_elements_by_css_selector("td.t-col-role")[i].click()
+                driver.find_elements_by_css_selector("td.t-col-edit-form > a")[0].click()
             except:
                 # hit the toggle to show the folder contents
                 driver.find_element_by_id("folder_2").click()
                 # retry
-                driver.find_elements_by_css_selector("td.t-col-role")[i].click()
+                driver.find_elements_by_css_selector("td.t-col-edit-form > a")[0].click()
             
-            # select correct box and type in netid
-            netid_input = driver.find_element_by_id(text_field_ids[self.new_user_type])
-            netid_input.clear()
-            netid_input.send_keys(self.new_user)
+            # click edit survey
+            driver.find_element_by_css_selector("input[value=\"Edit Survey\"]").click()
             
-            # submit the netid
-            driver.find_element_by_id(button_ids[self.new_user_type]).click()
-        
             # Go back to the setup page
-            driver.find_element_by_css_selector("#back > input").click()
+            driver.find_element_by_css_selector("input[value=\"Back\"]").click()
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
