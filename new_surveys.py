076d28e10572632670b989fc163a579b31caeefd
David Goldstein <dgoldstein0@gmail.com>
2013-04-13 02:56:25 -0500

    autogenerating surveys\!\! I think this will work, but it still needs docs / to be run through some more complex scenarios.  Only allows a subset of toolbox features at the moment.

---
+++ new_surveys.py
@@ -0,0 +1,86 @@
+#! python
+
+from survey_script import SurveyScript
+
+import survey
+
+class NewSurveys(SurveyScript):
+    def __init__(self):
+        
+	self.surveys = survey.make_surveys()
+	
+        super(NewSurveys, self).setUp()
+    
+    def make_new_surveys(self):
+        driver = self.driver
+        driver.get(self.base_url + "toolbox")
+        
+        # Authenticate
+        self.authenticate()
+
+	for i, survey in enumerate(self.surveys):
+		try:
+			# Go to survey setup tab and click create
+			driver.find_element_by_id("survey-setup").click()
+			driver.find_element_by_link_text("Create").click()
+			
+			# Enter a name for the survey
+			survey_name = driver.find_element_by_id("name")
+			survey_name.clear()
+			survey_name.send_keys(survey.name)
+			
+			# Save it
+			driver.find_element_by_css_selector("input[value=\"Save\"]").click()
+			
+			if (survey.submit_once):
+				# go to security tab
+				driver.find_element_by_link_text("Security").click()
+				driver.find_element_by_css_selector("div.pc-form-row > div.general-action-list a.opt-not-selected").click()
+			
+			driver.find_element_by_link_text("Questions").click()
+			
+			# Now add questions
+			for j, question in enumerate(sorted(survey.questions, key=lambda q : q.rank)):
+				if (j == 0):
+					driver.find_element_by_link_text("insert new question").click()
+				else:
+					# use the last add question button, so our new question goes last
+					driver.find_elements_by_link_text("add question")[-1].click()
+				
+				q_field = driver.find_element_by_css_selector("#question")
+				q_field.clear()
+				q_field.send_keys(question.question)
+				
+				if (question.required):
+					driver.find_element_by_css_selector("#response_yes").click()
+				
+				if (question.type == "Short Answer"):
+					# short answer is default
+					pass
+				elif (question.type == "Long Answer"):
+					driver.find_element_by_link_text("Long Answer").click()
+				elif (question.type == "Radio" or question.type == "Checkbox"):
+					# Remember todo Other
+					driver.find_element_by_link_text(question.type).click()
+					
+					if "Other" in question.answers:
+						driver.find_element_by_link_text("yes").click()
+						
+					answers = filter(lambda x: x != 'Other', question.answers)
+					
+					answer_box = driver.find_element_by_css_selector("#addAnswers")
+					answer_box.clear()
+					answer_box.send_keys('\n'.join(answers))
+				
+				driver.find_element_by_css_selector("input[value=\"Save\"]").click()
+				driver.find_element_by_css_selector("input[value=\"Back\"]").click()
+			
+			driver.find_element_by_css_selector("input[value=\"Save\"]").click()
+			driver.find_element_by_css_selector("input[value=\"Back\"]").click()
+				
+		except Exception:
+			print "Warning: exception encountered while generating survey %s.  Delete the survey and restart on survey #%s." % (survey.name, i)
+			raise
+
+if __name__ == "__main__":
+    NewSurveys().make_new_surveys()
---
+++ survey.py
@@ -0,0 +1,120 @@
+#! python
+
+import csv, sys, json
+
+class Question:
+    def __init__(self, question, type, answers, rank, required=False):
+        '''constructor for Question object
+        
+        question: the question, as a string
+        type: the type of question, e.g. Checkbox, Radio, Long Answer, or Short Answer
+        answers: list of possible answers.  Note "Other" is treated specially.
+        rank: used to sort the questions
+        required: whether or not the question is required.  Default is not required.
+        '''
+        self.required = required
+        self.question = question
+        self.type = type
+        self.answers = answers
+        self.rank = rank
+
+class Survey:
+    def __init__(self, name, submit_once):
+        self.name = name
+        self.questions = []
+        self.submit_once = submit_once
+        
+    def add_question(self, q):
+        self.questions.append(q)
+
+def make_surveys():
+    '''open up the files and generate surveys from them'''
+    
+    assert len(sys.argv) == 3, "Usage: %s <survey_roster> <survey_template>" % sys.argv[0]
+    
+    with open(sys.argv[1], 'r') as survey_roster:
+        roster_csv = [line for line in csv.reader(survey_roster)]
+    
+    with open(sys.argv[2], 'r') as template_file:
+        template_csv = [line for line in csv.reader(template_file)]
+    
+    roster_header = roster_csv[0]
+    roster_rows = roster_csv[1:]
+    
+    return [ instantiate_template(template_csv, zip(roster_header, roster_line))
+                for roster_line in roster_rows ]
+
+def yesno_to_bool(input, error_in):
+    '''Takes in input, returns True for yes, False for no.  If input is neither of these things,
+    an error will be raised, with error_in as part of the error message.'''
+    if input == 'yes':
+        return True
+    elif input == 'no':
+        return False
+    else:
+        assert False, "Value '%s' for %s should be yes or no" % (repr(input), error_in)    
+
+def instantiate_template(template_csv, survey_info):
+    '''Does all the actual interpretation of the csv data.  Returns a Survey.'''
+    
+    # first line of each file is a header that tells us how to interpret it.
+    template_header = template_csv[0]
+    template_rows = template_csv[1:]
+    
+    # read the row from the survey roster that describes this survey
+    custom_answers = {}
+    custom_questions = []
+    for (col, datum) in survey_info:
+        if col == "Name":
+            name = datum
+        elif col == "single submission":
+            submit_once = yesno_to_bool(datum, 'single submission')
+        elif col == "Custom":
+            # custom question; multiple of these are allowed.
+            if datum: # ignore empty strings
+                try:
+                    custom_questions.append(Question(**json.loads(datum)))
+                except Exception as e:
+                    print 'Error loading custom question %s' % datum
+                    raise
+        elif col.endswith(": answers"):
+            # custom answers
+            custom_answers[col.partition(':')[0]] = datum
+        else:
+            assert False, "Bad column name in survey roster: %s" % col
+    
+    survey = Survey(name, submit_once)
+    
+    # verify template validity
+    # Going for a very strict format for easier parsing.  Columns must be in specified order.
+    assert len(template_header) == 6, "Missing column in template!"
+    for colhead, shouldbe in zip(template_header, ['Id', 'Questions', 'Type', 'Required', 'Answers', 'Rank']):
+        assert colhead == shouldbe, "Invalid column in template: %s" % colhead
+    
+    for line in template_rows:
+        id = line[0]
+        question = line[1]
+        type = line[2]
+        required = yesno_to_bool(line[3], 'required')
+        
+	assert type in ['Long Answer', 'Short Answer', 'Radio', 'Checkbox'] , "Question type %s is not supported" % type
+	
+        # deal with custom answers per survey.
+        # note we check one of the two error cases - where the survey roster is missing the column for
+        # custom answers; the other case, where the survey roster has extra columns, is unlikely to happen;
+        # if it does, it will most likely be a mislabeled column.
+        raw_answers = custom_answers.get(id) if (line[4] == '#CUSTOM#') else line[4]
+        assert raw_answers is not None, "Survey roster is missing the column for custom answers for question %s" % id
+        
+        answers = map(lambda x: x.strip(), raw_answers.split(',')) # strip removes excess whitespace
+        rank = int(line[5])
+        
+        survey.add_question(Question(question, type, answers, rank, required))
+    
+    # add in custom questions
+    map(survey.add_question, custom_questions)
+    
+    return survey
+
+if __name__ == '__main__':
+    make_surveys()
