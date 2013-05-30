9f72edcf9d6ff0675e0bfcbf5da8364b6b613fde
David Goldstein <dgoldstein0@gmail.com>
2013-05-29 21:55:18 -0500

    updated survey_stats.py to take the path to the directory to look in as a command line argument

--- survey_stats.py
+++ survey_stats.py
@@ -1,16 +1,25 @@
 #! python
 
-import os, csv
+import os, csv, sys
 
-data_path = "../Fall 2012 Survey Data"
+def print_stats(data_path):
+    files = [
+                os.path.join(data_path, f) for f in os.listdir(data_path)
+            ]
+    files = [f for f in files if os.path.isfile(f)]
 
-files = [
-            os.path.join(data_path, f) for f in os.listdir(data_path)
-        ]
-files = [f for f in files if os.path.isfile(f)]
+    for filename in files:
+        if filename.endswith(".csv"):
+            with open(filename, "r") as file:
+                lines = [l for l in csv.reader(file)]
+                print len(lines) - 1, "responses in", filename
 
-for filename in files:
-    if filename.endswith(".csv"):
-        with open(filename, "r") as file:
-            lines = [l for l in csv.reader(file)]
-            print len(lines) - 1, "responses in", filename
+def main():
+    if len(sys.argv) != 2:
+        print "Usage:", sys.argv[0], "<path of survey data>"
+        return
+
+    print_stats(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
